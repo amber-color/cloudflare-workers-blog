4a98dbbc43bc942716e5cd0d1ccd5dfb9a756f0e
amber-color <144450971+amber-color@users.noreply.github.com>
2026-05-24 00:32:40 +0900

    Update worker.js configuration

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 
 const OPT = {
     "user": "admin",
-    "password": "amepass",
+    "password": "amepasuwa-do",
     "siteDomain": "blog.oshekher.workers.dev",
     "siteName": "CF Workers Blog",
     "siteDescription": "A Blog Powered By Cloudflare Workers and KV",
